bcec29f59114ea627dde13f03c63b10207e20607
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-10-20 17:04:53 -0400

    feat: initialized repo ✨

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Condo Odoo Api Client</h1>
+
+<p align="center">A very lovely package. Hooray!</p>
+
+<p align="center">
+	<a href="https://github.com/vvelc/condo-odoo-api-client/blob/main/.github/CODE_OF_CONDUCT.md" target="_blank"><img alt="🤝 Code of Conduct: Kept" src="https://img.shields.io/badge/%F0%9F%A4%9D_code_of_conduct-kept-21bb42" /></a>
+	<a href="https://github.com/vvelc/condo-odoo-api-client/blob/main/LICENSE.md" target="_blank"><img alt="📝 License: MIT" src="https://img.shields.io/badge/%F0%9F%93%9D_license-MIT-21bb42.svg"></a>
+	<a href="http://npmjs.com/package/condo-odoo-api-client"><img alt="📦 npm version" src="https://img.shields.io/npm/v/condo-odoo-api-client?color=21bb42&label=%F0%9F%93%A6%20npm" /></a>
+	<img alt="💪 TypeScript: Strict" src="https://img.shields.io/badge/%F0%9F%92%AA_typescript-strict-21bb42.svg" />
+</p>
+
+## Usage
+
+```shell
+npm i condo-odoo-api-client
+```
+
+```ts
+import { greet } from "condo-odoo-api-client";
+
+greet("Hello, world! 💖");
+```
+
+<!-- You can remove this notice if you don't want it 🙂 no worries! -->
+
+> 💙 This package was templated with [`create-typescript-app`](https://github.com/JoshuaKGoldberg/create-typescript-app).
---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import eslint from "@eslint/js";
+import n from "eslint-plugin-n";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+	{
+		ignores: ["lib", "node_modules", "pnpm-lock.yaml", "**/*.snap"],
+	},
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: "error",
+		},
+	},
+	eslint.configs.recommended,
+	n.configs["flat/recommended"],
+	...tseslint.config({
+		extends: tseslint.configs.recommendedTypeChecked,
+		files: ["**/*.js", "**/*.ts"],
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ["*.*s", "eslint.config.js"],
+					defaultProject: "./tsconfig.json",
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			// These on-by-default rules don't work well for this repo and we like them off.
+			"no-constant-condition": "off",
+
+			// These on-by-default rules work well for this repo if configured
+			"@typescript-eslint/no-unused-vars": ["error", { caughtErrors: "all" }],
+		},
+	}),
+	{
+		files: ["*.jsonc"],
+		rules: {
+			"jsonc/comma-dangle": "off",
+			"jsonc/no-comments": "off",
+			"jsonc/sort-keys": "error",
+		},
+	},
+	{
+		extends: [tseslint.configs.disableTypeChecked],
+		files: ["**/*.md/*.ts"],
+		rules: {
+			"n/no-missing-import": [
+				"error",
+				{ allowModules: ["condo-odoo-api-client"] },
+			],
+		},
+	},
+);
